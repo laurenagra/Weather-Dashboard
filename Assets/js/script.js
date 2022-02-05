f6e33c4e4f939cd702b81afe18e485f9629bd399
Lauren A <laurenhaileyama@gmail.com>
2022-02-04 20:11:29 -0500

    push changes on js file

---
+++ Assets/js/script.js
@@ -0,0 +1,36 @@
+// Weather App: 
+
+// //Search 
+// Function: Event Handler
+// 1. Read value from the textbox into a variable 
+// 2. Creat URL for fetch
+// 3. Fetch results
+// 4. Call function to update after results
+
+// Function: DoResults
+// 1. Call function AddSearchTermtoResults
+// 2. Call function AddTodaysResults
+// 3. Call function Add5DayResults
+// 4. Clear textbox
+
+
+// //Recent Searches
+// Function: AddSearchTermtoResults
+// //Parameter in Search Term
+// 1. Add a button to the screen with a data attribute that stores the search terms
+
+// Function: Event Listener
+// 1. Read the data attribute to get the search term. 
+// 2. Call search function 
+
+// //Results Today
+// Function: AddToday'sResults
+
+// //Results 5Days
+// Function: Add 5day results
+
+// 1. 
+
+//Results today
+
+//Results 5 Days
